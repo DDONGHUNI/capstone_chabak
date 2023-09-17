0cc42dc0f06c0ab391ea93193382403f1b60d7dd
donghun <mi1232944@naver.com>
2023-09-17 16:38:41 +0900

    first commit

---
+++ src/main/java/com/example/chabak/ChabakApplication.java
@@ -0,0 +1,13 @@
+package com.example.chabak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChabakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChabakApplication.class, args);
+    }
+
+}
